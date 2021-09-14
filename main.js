6259ad0b121a4783debd94358799e3717a6c4af7
Your Name <you@example.com>
2021-09-14 18:25:47 -0500

    Test Commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client ();
+
+
+
+
+client.once('ready', () => {
+    console.log('I awaken.');
+});
+
+client.on('message', message =>{
+    if(message.author.bot)
+    return;
+
+    if(message.context.toLowerCase() === '@Music guy#6413')
+    message.channel.send("I will beat you to death.");
+});
+
+
+client.login('ODg3MTA3ODE2NDg0MTQzMTk0.YT_ViQ.oyuzZv3Hl1e1P5--iNEJ9tNWgmY');
